cf880cd76ecf0034ce6696959b681ca901d157cc
Jakob Polegek <jakobpolegek@hotmail.com>
2025-05-28 19:25:23 +0200

    -added auth controller -added functions for sign-up and sign-in
    Signed-off-by: Jakob Polegek <jakobpolegek@hotmail.com>

---
+++ cratco_backend/controllers/auth.controller.js
@@ -0,0 +1,61 @@
+import mongoose from 'mongoose';
+import User from "../models/user.mode.js";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import {JWT_EXPIRES_IN, JWT_SECRET} from "../config/env.js";
+
+export const signUp = async (req, res, next) => {
+    const session = await mongoose.startSession();
+    session.startTransaction();
+
+    try {
+        const {name, email, password} = req.body;
+        const existingUser = await User.findOne({email});
+        if (existingUser) {
+            const error = new Error('User already exists');
+            error.status = 409;
+            throw error;
+        }
+        const salt = await bcrypt.genSalt(12);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        const newUsers = await User.create([{name, email, password: hashedPassword}], {session});
+        const token = jwt.sign({userId: newUsers[0]._id}, JWT_SECRET, {expiresIn: JWT_EXPIRES_IN});
+
+        await session.commitTransaction();
+        session.endSession();
+
+        res.status(201).json({success: true, message: 'User created successfully!', data: {token, user: newUsers[0]}});
+    } catch (error) {
+        await session.abortTransaction();
+        session.endSession();
+        next(error);
+    }
+}
+
+export const signIn = async (req, res, next) => {
+    try {
+        const {email, password} = req.body;
+        const user = await User.findOne({email});
+        if (!user) {
+            const error = new Error('Invalid credentials.');
+            error.status = 401;
+            throw error;
+        }
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+            const error = new Error('Invalid credentials.');
+            error.status = 401;
+            throw error;
+        }
+
+        const token = jwt.sign({userId: user._id}, JWT_SECRET, {expiresIn: JWT_EXPIRES_IN});
+        res.status(200).json({success: true, message: 'User logged in successfully!', data: {token, user}});
+    } catch (error) {
+        next(error);
+    }
+}
+
+/*export const signOut = async (req, res, next) => {
+
+}*/
