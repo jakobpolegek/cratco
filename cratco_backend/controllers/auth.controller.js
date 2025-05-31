24044e799e82037faeecf4d1c99c5f7b3cb88883
Jakob Polegek <jakobpolegek@hotmail.com>
2025-05-31 06:39:43 +0200

    -added signOut function
    Signed-off-by: Jakob Polegek <jakobpolegek@hotmail.com>

--- cratco_backend/controllers/auth.controller.js
+++ cratco_backend/controllers/auth.controller.js
@@ -56,6 +56,10 @@ export const signIn = async (req, res, next) => {
     }
 }
 
-/*export const signOut = async (req, res, next) => {
-
-}*/
+export const signOut = async (req, res, next) => {
+    try {
+        res.status(200).json({success: true, message: 'User logged out successfully!'});
+    } catch (error) {
+        next(error);
+    }
+}
--- cratco_backend/routes/auth.routes.js
+++ cratco_backend/routes/auth.routes.js
@@ -1,10 +1,10 @@
 import {Router} from 'express';
-import {signIn, signUp} from "../controllers/auth.controller.js";
+import {signIn, signOut, signUp} from "../controllers/auth.controller.js";
 
 const authRouter = Router();
 
 authRouter.post('/sign-up', signUp);
 authRouter.post('/sign-in', signIn)
-//authRouter.post('/sign-out', signOut)
+authRouter.post('/sign-out', signOut)
 
 export default authRouter;
